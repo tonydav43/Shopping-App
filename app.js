47fa0124e8dd4b87464e247f4ccf0ea595c99cf0
Antony Davenport <tonydav43@hotmail.co.uk>
2020-03-30 21:28:06 +0000

    change listening port

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const bodyParser    = require("body-parser");
 const expressSanitizer  = require("express-sanitizer");
 const methodOverride  = require('method-override');
 
-//mongoose.connect("mongodb://localhost:27017/shopping-app", {useMongoClient:true});
-let url = process.env.DATABASEURL || "mongodb://localhost:27017/shopping-app";
-mongoose.connect(url, {useMongoClient:true});
+mongoose.connect("mongodb://localhost:27017/shopping-app", {useMongoClient:true});
+/*let url = process.env.DATABASEURL || "mongodb://localhost:27017/shopping-app";
+mongoose.connect(url, {useMongoClient:true});*/
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSanitizer());
@@ -85,10 +85,10 @@ app.delete("/todos/:id", function (req, res) {
  }); 
 });
 
-app.listen(process.env.PORT || 3000,function () {
+/*app.listen(process.env.PORT || 3000,function () {
   console.log("Server running shopping app on port 3000 " + process.env.PORT);
-});
-
-/*app.listen(3000, () =>{
-  console.log('Server running shopping app on port 3000');
 });*/
+
+app.listen(3000, function () {
+  console.log('Server running shopping app on port 3000');
+});
